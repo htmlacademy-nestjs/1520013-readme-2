d20b0187b60d8ce037a9e26608d8268bb274623d
VladimirDyachenko <d.vovan3@gmail.com>
2025-03-20 01:32:03 +0300

    Подключил модуль постов в основное приложение

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogTagModule } from './blog-tag/blog-tag.module';
 import { BlogCommentModule } from './blog-comment/blog-comment.module';
+import { BlogPostModule } from './blog-post/blog-post.module';
 
 @Module({
-  imports: [BlogTagModule, BlogCommentModule],
+  imports: [BlogTagModule, BlogCommentModule, BlogPostModule],
   controllers: [],
   providers: [],
 })
